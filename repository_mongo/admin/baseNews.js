78b1cdb69891027889ad4df1cef89faf13551ffc
thzero <thzero@users.noreply.github.com>
2020-07-17 23:02:11 -0500

    refactor for seperation of dependencies

--- repository_mongo/admin/baseNews.js
+++
@@ -1,9 +0,0 @@
-import BaseAdminMongoRepository from './index';
-
-class NewsBaseAdminMongoRepository extends BaseAdminMongoRepository {
-	async _getCollectionAdmin() {
-		return await this._getCollectionFromConfig(this._collectionsConfig.getCollectionNews());
-	}
-}
-
-export default NewsBaseAdminMongoRepository;
