5f8418c9190361079e2a23c86dec84c7fbedeed2
thzero <thzero@users.noreply.github.com>
2020-07-21 19:00:06 -0500

    sync updates

--- utility/index.js
+++ utility/index.js
@@ -3,90 +3,103 @@ import shortUUID from 'short-uuid';
 import { v4 as uuidv4 } from 'uuid';
 import _cloneDeep from 'lodash/cloneDeep';
 import _debounce from 'lodash/debounce';
-import _merge from 'lodash/merge'
+import _merge from 'lodash/merge';
 
 import LibraryConstants from '../constants';
 
 import CryptoUtility from '../utility/crypto';
 
-import Response from '../response/index'
+import Response from '../response/index';
 
 const uuidTranslator = shortUUID();
 
 class Utility {
 	static async checksumUpdateCheck(state, commit, name, params) {
-		const internal = {}
-		internal.name = name
-		internal.params = params
-		const checksum = await CryptoUtility.checksum(internal)
+		const internal = {};
+		internal.name = name;
+		internal.params = params;
+		const checksum = await CryptoUtility.checksum(internal);
 
-		const temp = state.checksumLastUpdate[checksum]
+		const temp = state.checksumLastUpdate[checksum];
 		if (!temp) {
 			// state.checksumLastUpdate[checksum] = Utility.getTimestamp()
 			// commit('setCheckumLastUpdate', state.checksumLastUpdate)
-			return false
+			return false;
 		}
 
-		const now = Utility.getTimestamp()
-		const delta = now - temp
-		const max = 5 * 1000 * 60
+		const now = Utility.getTimestamp();
+		const delta = now - temp;
+		const max = 5 * 1000 * 60;
 		if (delta > max) {
 			// state.checksumLastUpdate[checksum] = Utility.getTimestamp()
 			// commit('setCheckumLastUpdate', state.checksumLastUpdate)
-			return false
+			return false;
 		}
 
-		return true
+		return true;
 	}
 
 	static checksumUpdateComplete(state, commit, name, params) {
-		const internal = {}
-		internal.name = name
-		internal.params = params
-		const checksum = CryptoUtility.checksum(internal)
-		state.checksumLastUpdate[checksum] = Utility.getTimestamp()
-		commit('setCheckumLastUpdate', state.checksumLastUpdate)
+		const internal = {};
+		internal.name = name;
+		internal.params = params;
+		const checksum = CryptoUtility.checksum(internal);
+		state.checksumLastUpdate[checksum] = Utility.getTimestamp();
+		commit('setCheckumLastUpdate', state.checksumLastUpdate);
 	}
 
 	static cloneDeep(value) {
-		return _cloneDeep(value)
+		return _cloneDeep(value);
 	}
 
 	static convertTimestampToLocal(value) {
-		const temp = dayjs(value)
-		temp.locale(navigator.language)
-		return temp.valueOf()
+		const temp = dayjs(value);
+		temp.locale(navigator.language);
+		return temp.valueOf();
 	}
 	static convertTimestampFromLocal(value) {
-		const temp = dayjs(value).utc()
-		return temp.valueOf()
+		const temp = dayjs(value).utc();
+		return temp.valueOf();
 	}
 
 	static convertTimestampSecondsToLocal(value) {
-		const temp = dayjs.unix(value)
-		temp.locale(navigator.language)
-		return temp.valueOf()
+		const temp = dayjs.unix(value);
+		temp.locale(navigator.language);
+		return temp.valueOf();
 	}
 	static convertTimestampSecondsFromLocal(value) {
-		const temp = dayjs.unix(value).utc()
-		return temp.valueOf()
+		const temp = dayjs.unix(value).utc();
+		return temp.valueOf();
 	}
 
 	static debounce(func, ttl) {
-		return _debounce(func, ttl)
+		return _debounce(func, ttl);
 	}
-
 
 	static deleteArrayById(array, id) {
 		if (!array)
-			return
+			return;
 		if (!id)
-			return
+			return;
 
 		const i = array.map(item => item.id).indexOf(id)
 		if (i === -1)
-			return
-		array.splice(i, 1)
+			return;
+		array.splice(i, 1);
+	}
+
+	static formatUrl(url) {
+		let urlF = url;
+		if (!(typeof url === 'string' || url instanceof String))
+			urlF = Utility.formatUrlParams(url.url, url.params);
+		return urlF;
+	}
+
+	static formatUrlParams(url, params) {
+		if (!Array.isArray(params))
+			params = params ? [ params ] : [];
+		params.unshift(url);
+		return params.join('/');
 	}
 
 	static generateId() {
@@ -104,7 +117,7 @@ class Utility {
 
 	static getAuthToken(context) {
 		if (!context)
-			return null
+			return null;
 
 		const token = context.get(LibraryConstants.Headers.AuthKeys.AUTH);
 		const split = token.split(LibraryConstants.Headers.AuthKeys.AUTH_BEARER + Utility.separator);
@@ -156,32 +169,32 @@ class Utility {
 	}
 
 	static getTimestampLocal() {
-		const temp = dayjs()
-		temp.locale(navigator.locale)
-		return temp.valueOf()
+		const temp = dayjs();
+		temp.locale(navigator.locale);
+		return temp.valueOf();
 	}
 
 	static getTimestampSeconds() {
-		const temp = dayjs().unix()
-		return temp.valueOf()
+		const temp = dayjs().unix();
+		return temp.valueOf();
 	}
 
 	static getTimestampSecondsLocal() {
-		const temp = dayjs().unix()
-		temp.locale(navigator.locale)
-		return temp.valueOf()
+		const temp = dayjs().unix();
+		temp.locale(navigator.locale);
+		return temp.valueOf();
 	}
 
 	static instantiate(object) {
 		if (!object)
-			return null
+			return null;
 
-		delete object.id
-		delete object.createdTimestamp
-		delete object.createdUserId
-		delete object.updatedUserId
-		delete object.updatedTimestamp
-		return object
+		delete object.id;
+		delete object.createdTimestamp;
+		delete object.createdUserId;
+		delete object.updatedUserId;
+		delete object.updatedTimestamp;
+		return object;
 	}
 
 	static get isDev() {
@@ -194,8 +207,8 @@ class Utility {
 	}
 
 	static map(obj1, obj2, resetTimestamps) {
-		if (!obj1 || !obj2)
-			return obj1
+		if (!obj1 || !obj2);
+			return obj1;
 
 		obj1.map(obj2);
 
@@ -208,12 +221,12 @@ class Utility {
 	}
 
 	static merge2(x, y) {
-		return _merge(x, y)
+		return _merge(x, y);
 		// return Object.assign(x, y)
 	}
 
 	static merge3(x, y, z) {
-		return _merge(x, y, z)
+		return _merge(x, y, z);
 		// return Object.assign(x, y, z)
 	}
 
@@ -225,7 +238,7 @@ class Utility {
 			id = setTimeout(() => {
 				reject(Response.error(`Timed out in ${ttl}ms.`));
 			}, ttl);
-		})
+		});
 
 		// Returns a race between our timeout and the passed in promise
 		return Promise.race([
@@ -235,31 +248,31 @@ class Utility {
 			clearTimeout(id)
 			// Pass the result back
 			return result
-		})
+		});
 	}
 
 	static sortByName(values, ascending) {
 		if (!values || !Array.isArray(values))
-			return values
+			return values;
 
 		if (ascending)
-			return values.sort((a, b) => Utility.sortByString(a, b, (v) => { return v ? v.name : null }))
+			return values.sort((a, b) => Utility.sortByString(a, b, (v) => { return v ? v.name : null }));
 
-		return values.sort((a, b) => Utility.sortByString(b, a, (v) => { return v ? v.name : null }))
+		return values.sort((a, b) => Utility.sortByString(b, a, (v) => { return v ? v.name : null }));
 	}
 
 	static sortByNumber(a, b, field) {
 		if (a && !b)
-			return 1
+			return 1;
 		if (!a && b)
-			return -1
+			return -1;
 		if (!a && !b)
-			return 0
+			return 0;
 		if (field) {
-			a = field(a)
-			b = field(b)
+			a = field(a);
+			b = field(b);
 		}
-		return (b - a)
+		return (b - a);
 	}
 
 	static sortByNumberEx(values, field, ascending) {
@@ -274,36 +287,36 @@ class Utility {
 
 	static sortByOrder(values, ascending) {
 		if (!values || !Array.isArray(values))
-			return values
+			return values;
 
 		if (ascending)
-			return values.sort((a, b) => Utility.sortByNumber(b, a, (v) => { return v ? v.order : null }))
+			return values.sort((a, b) => Utility.sortByNumber(b, a, (v) => { return v ? v.order : null }));
 
-		return values.sort((a, b) => Utility.sortByNumber(a, b, (v) => { return v ? v.order : null }))
+		return values.sort((a, b) => Utility.sortByNumber(a, b, (v) => { return v ? v.order : null }));
 	}
 
 	static sortByString(a, b, field) {
 		if (a && !b)
-			return 1
+			return 1;
 		if (!a && b)
-			return -1
+			return -1;
 		if (!a && !b)
-			return 0
+			return 0;
 		if (field) {
-			a = field(a)
-			b = field(b)
+			a = field(a);
+			b = field(b);
 		}
-		return (a && a.localeCompare(b))
+		return (a && a.localeCompare(b));
 	}
 
 	static sortByTimestamp(values, ascending) {
 		if (!values || !Array.isArray(values))
-			return values
+			return values;
 
 		if (ascending)
-			return values.sort((a, b) => Utility.sortByNumber(a, b, (v) => { return v ? v.timestamp : null }))
+			return values.sort((a, b) => Utility.sortByNumber(a, b, (v) => { return v ? v.timestamp : null }));
 
-		return values.sort((a, b) => Utility.sortByNumber(b, a, (v) => { return v ? v.timestamp : null }))
+		return values.sort((a, b) => Utility.sortByNumber(b, a, (v) => { return v ? v.timestamp : null }));
 	}
 
 	static stringify(value) {
@@ -323,21 +336,21 @@ class Utility {
 	}
 
 	static translateToShortId(id) {
-		return uuidTranslator.fromUUID(id)
+		return uuidTranslator.fromUUID(id);
 	}
 
 	static translateToId(id) {
-		return uuidTranslator.toUUID(id)
+		return uuidTranslator.toUUID(id);
 	}
 
 	static update(object) {
 		if (!object)
-			return null
+			return null;
 
-		delete object.createdTimestamp
-		delete object.createdUserId
-		delete object.updatedUserId
-		return object
+		delete object.createdTimestamp;
+		delete object.createdUserId;
+		delete object.updatedUserId;
+		return object;
 	}
 
 	static _replacer(key, value) {
@@ -347,7 +360,7 @@ class Utility {
 		return value;
 	}
 
-	static separator = ': '
+	static separator = ': ';
 }
 
 export default Utility;
