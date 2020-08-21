e710f72afabbecbaae66bb2b5c16369c45ba14cc
thzero <thzero@users.noreply.github.com>
2020-08-21 07:30:20 -0500

    refactor to move response base functions to Repository base

--- repository/index.js
+++ repository/index.js
@@ -12,6 +12,43 @@ class Repository {
 	get _logger() {
 		return this._injector.getService(LibraryConstants.InjectorKeys.SERVICE_LOGGER)
 	}
+
+	_error(message, err, code, errors) {
+		if (message)
+			this._logger.error(message);
+		if (err)
+			this._logger.error(err.message);
+		if (code)
+			this._logger.error(code);
+		if (errors)
+			this._logger.error(errors);
+		return Response.error(message, err, code, errors);
+	}
+
+	_errorResponse(response) {
+		if (!response)
+			return Response.error();
+
+		return Response.error(response.message, response.err, response.code, response.errors);
+	}
+
+	_initResponse() {
+		return new Response();
+	}
+
+	_initResponseExtract() {
+		return new ExtractResponse();
+	}
+
+	_success() {
+		return this._initResponse();
+	}
+
+	_successResponse(value) {
+		let response = Response.success();
+		response.results = value;
+		return response;
+	}
 }
 
 export default Repository;
