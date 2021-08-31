fd8fb72f6fb8fe9716705b0b29703d23ede2e6f4
thzero <corvette@thzero.com>
2021-08-31 07:39:38 -0500

    add os utility

---
+++ utility/os.js
@@ -0,0 +1,22 @@
+import os from 'os';
+
+const uuidTranslator = shortUUID();
+
+class OsUtility {
+	static get isLinux() {
+		const type = os.type();
+		return (/^linux/i.test(type) || /^freebsd/i.test(type) || /^darwin/i.test(type));
+	}
+
+	static get isMac() {
+		const type = os.type();
+		return (/^darwin/i.test(type));
+	}
+
+	static get isWin() {
+		const type = os.type();
+		return (/^win/i.test(type));
+	}
+}
+
+export default OsUtility;
